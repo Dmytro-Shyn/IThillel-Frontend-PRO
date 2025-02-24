6d852b0717d1b5a7809d2fa59e7ddeeb33d98fa3
Dmytro Shynkarenko <dmytroshynkarenko26@gmail.com>
2025-02-24 19:31:08 +0200

    Refactor form and error handling in HTML and JavaScript, style updates

--- lesson_13/homework_13.1/script.js
+++ lesson_13/homework_13.1/script.js
@@ -1,31 +1,93 @@
 const form = document.getElementById("userForm");
-const messageInput = document.getElementById("textarea");
+// const messageInput = document.getElementById("textarea");
+const inputUserName = document.getElementById("userName");
+const inputTextArea = document.getElementById("textArea");
+const inputTel = document.getElementById("phone");
 
-const errorMessage = document.createElement("p");
-errorMessage.style.position = "absolute";
-errorMessage.style.bottom = "5px";
-errorMessage.style.color = "red";
-errorMessage.style.fontSize = "14px";
-errorMessage.style.display = "none"; // Ховаємо помилку на початку
-messageInput.insertAdjacentElement("afterend", errorMessage);
+// REDEX
+const nameRegex = /^[a-zA-Zа-яА-ЯіІїЇєЄ]{5,20}$/;
+const massageRegex = /^.{5,}$/;
+const phoneRegex = /^\+380\d{9}$/;
 
-messageInput.addEventListener("input", () => {
-  const messageValue = messageInput.value;
+// Error Massage
+const formErrorUserName = "Ім'я повинно містити тільки літери (5-20 символів)";
+const formErrorMassage = "Повідомлення повинно містити мінімум (5 символів)";
+const formErrorTel = "Невірний формат номера. Введіть у форматі +380XXXXXXXXX";
 
-  if (!/^.{5,}$/.test(messageValue)) {
-    errorMessage.textContent = "Message must be at least 5 characters long.";
-    errorMessage.style.display = "block";
+//userName
+inputUserName.addEventListener("change", () => {
+  console.log(inputUserName.value.length);
+
+  if (!nameRegex.test(inputUserName.value.trim())) {
+    inputUserName.classList.add("form-input-invalid");
+    form.children[0].querySelector("div").style.display = "inline-block";
+    form.children[0].querySelector("div").children[0].textContent =
+      formErrorUserName;
   } else {
-    errorMessage.style.display = "none";
+    inputUserName.classList.remove("form-input-invalid");
+    form.children[0].querySelector("div").style.display = "none";
+    inputUserName.oninvalid;
+  }
+});
+// massage
+inputTextArea.addEventListener("change", () => {
+  console.log(inputTextArea.value.length);
+
+  if (!massageRegex.test(inputTextArea.value.trim())) {
+    inputTextArea.classList.add("form-input-invalid");
+    form.children[1].querySelector("div").style.display = "inline-block";
+    form.children[1].querySelector("div").children[0].textContent =
+      formErrorMassage;
+  } else {
+    inputTextArea.classList.remove("form-input-invalid");
+    form.children[1].querySelector("div").style.display = "none";
+    inputTextArea.oninvalid;
+  }
+});
+// tell
+inputTel.addEventListener("input", function () {
+  this.value = this.value.replace(/\D/g, "");
+});
+inputTel.addEventListener("click", function () {
+  if (this.value === "") {
+    // Якщо поле порожнє
+    this.value = "+380"; // Вставити +380
   }
 });
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+inputTel.addEventListener("change", () => {
+  console.log(inputTel.value.length);
 
-  const formData = new FormData(form);
-
-  for (const [name, value] of formData.entries()) {
-    console.log(`${name}: ${value}`);
+  if (!phoneRegex.test(inputTel.value.trim())) {
+    inputTel.classList.add("form-input-invalid");
+    form.children[3].querySelector("div").style.display = "inline-block";
+    form.children[3].querySelector("div").children[0].textContent =
+      formErrorTel;
+  } else {
+    inputTel.classList.remove("form-input-invalid");
+    form.children[3].querySelector("div").style.display = "none";
+    inputTel.oninvalid;
   }
 });
+console.log(form.children[3]);
+console.log(form.children);
+// messageInput.addEventListener("input", () => {
+//   const messageValue = messageInput.value;
+
+//   if (!/^.{5,}$/.test(messageValue)) {
+//     errorMessage.textContent = "Message must be at least 5 characters long.";
+//     errorMessage.style.display = "block";
+//   } else {
+//     errorMessage.style.display = "none";
+//   }
+// });
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const formData = new FormData(form);
+
+//   for (const [name, value] of formData.entries()) {
+//     console.log(`${name}: ${value}`);
+//   }
+// });
--- lesson_14/homework_14.1/script.js
+++ lesson_14/homework_14.1/script.js
@@ -1,34 +1,62 @@
+const images = [
+  "https://plus.unsplash.com/premium_photo-1728312313640-5d86582dcca7?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1739614621579-8f8f396c7412?q=80&w=2673&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1720020552784-f675b06605f8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
 const buttonNext = document.getElementById("btn-1");
 const buttonPrev = document.getElementById("btn-2");
 const foto = document.getElementById("foto");
+const dots = document.getElementById("wr-dots");
 
-let num = 1; // Лічильник зображень
-const maxNum = 10; // Максимальна кількість зображень
+let imgIdx = 0;
 
-function updateSlider() {
-  foto.src = `./foto/${num}.jpg`;
-  foto.alt = `Image ${num}`;
+const updateSlider = () => {
+  dots.children[0].classList.remove("dots-active");
+  dots.children[1].classList.remove("dots-active");
+  dots.children[2].classList.remove("dots-active");
+  foto.src = images[imgIdx];
+  buttonPrev.disabled = imgIdx === 0;
+  buttonNext.disabled = imgIdx === images.length - 1;
 
-  buttonNext.disabled = num === maxNum;
-  buttonPrev.disabled = num === 1;
-}
+  if (foto.attributes[1].textContent == images[0]) {
+    dots.children[0].classList.add("dots-active");
+  } else if (foto.attributes[1].textContent == images[1]) {
+    dots.children[1].classList.add("dots-active");
+  } else if (foto.attributes[1].textContent == images[2]) {
+    dots.children[2].classList.add("dots-active");
+  }
 
-function slaider() {
+  console.log(foto.attributes[1].textContent);
+  console.log(images[0]);
+};
+
+const slider = () => {
   buttonNext.addEventListener("click", () => {
-    if (num < maxNum) {
-      num++;
+    if (imgIdx < images.length - 1) {
+      imgIdx++;
       updateSlider();
     }
   });
 
   buttonPrev.addEventListener("click", () => {
-    if (num > 1) {
-      num--;
+    if (imgIdx >= 0) {
+      imgIdx--;
       updateSlider();
     }
   });
 
-  updateSlider(); // Оновлюємо стан кнопок при завантаженні
-}
+  dots.addEventListener("click", (event) => {
+    let dotsArray = Array.from(document.querySelectorAll(".dots"));
+    let clickedDot = event.target;
 
-slaider();
+    if (clickedDot.classList.contains("dots")) {
+      imgIdx = dotsArray.indexOf(clickedDot);
+      updateSlider();
+    }
+  });
+
+  updateSlider();
+};
+
+slider();
